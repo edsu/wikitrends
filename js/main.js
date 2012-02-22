137d97433d4fb792bde8618fbcf6f7608e13d472
Ed Summers <ehs@pobox.com>
2012-02-22 02:30:30 +0000

    align numbers to the right ; also check every 5 minutes for updated stats

--- js/main.js
+++ js/main.js
@@ -3,9 +3,7 @@ $(document).ready(main);
 var gettingArticle = false;
 
 function main() {
-  var t = new Date();
-  t.setHours(t.getHours() - 1);
-  loadStats(t);
+  loadStats();
 }
 
 function loadStats() {
@@ -18,6 +16,9 @@ function loadStats() {
     t.setHours(t.getHours() + 1);
   }
   async.map(hours, load, display);
+
+  // check back in 5 minutes for more latest stats
+  setTimeout(loadStats, 5 * 1000);
 }
 
 function load(t, callback) {
@@ -44,7 +45,7 @@ function display(err, stats) {
       }
       var viewsPerSec = _.str.sprintf("%0.2f", (row.count / 60 / 60));
       // yeah, maybe this should be a template of some kind eh? /me shrugs
-      $("#articles").append('<tr class="article"><td>' + (i+1) + '</td><td>' + prevHourRank + '</td><td title="' + viewsPerSec + ' page views per second">' + row.count + '</td><td><a target="_blank" class="article" href="http://en.wikipedia.org/wiki/' + row.page + '">' + row.page +'</a></td><td><a target="_blank" href="http://en.wikipedia.org/wiki/' + row.page + '"><img class="icon" src="images/wikipedia.jpg"></a><a target="_blank" href="https://twitter.com/#!/search/realtime/' + row.page + '"><img class="icon" src="images/twitter.jpg"></a><a target="_blank" href="https://www.google.com/search?tbs=qdr:h&q=' + row.page + '"><img class="icon" src="images/google.jpg"></a><a target="_blank" href="https://www.facebook.com/search/results.php?type=web&q=' + row.page + '"><img class="icon" src="images/facebook.jpg"></a></td></tr>');  
+      $("#articles").append('<tr class="article"><td class="rank">' + (i+1) + '</td><td class="prevRank">' + prevHourRank + '</td><td class="views" title="that\'s ' + viewsPerSec + ' views per second">' + row.count + '</td><td><a target="_blank" class="article" href="http://en.wikipedia.org/wiki/' + row.page + '">' + row.page +'</a></td><td class="social"><a title="View Wikipedia Article" target="_blank" href="http://en.wikipedia.org/wiki/' + row.page + '"><img class="icon" src="images/wikipedia.jpg"></a><a title="Twitter Real Time Search" target="_blank" href="https://twitter.com/#!/search/realtime/' + row.page + '"><img class="icon" src="images/twitter.jpg"></a><a title="Google Real Time Search" target="_blank" href="https://www.google.com/search?tbs=qdr:h&q=' + row.page + '"><img class="icon" src="images/google.jpg"></a><a title="Facebook Search" target="_blank" href="https://www.facebook.com/search/results.php?type=web&q=' + row.page + '"><img class="icon" src="images/facebook.jpg"></a></td></tr>');  
   });
   $("a.article").mouseover(showArticle);
   $("a.article").mouseout(hideArticle);
